6f8ec1031480efa07b4df1023d0318d97d7fc4ea
allinuxwork <altim4@yandex.ru>
2020-07-14 16:32:54 +0400

    init lesson_3_hw selenium junit

---
+++ src/participants/Runner.java
@@ -0,0 +1,19 @@
+package participants;
+
+public interface Runner {
+
+    String getName();
+
+    int getMaxLength();
+
+    int getMaxWeight();
+
+    boolean getSuccess();
+
+    void setSuccess(boolean success);
+
+    void run();
+
+    void jump();
+
+}
