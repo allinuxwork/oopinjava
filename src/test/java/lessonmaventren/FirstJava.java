69e26d28986a577351561e5e8be42d96b2d6a28e
allinuxwork <altim4@yandex.ru>
2020-07-18 07:05:27 +0400

    First Test

---
+++ src/test/java/lessonmaventren/FirstJava.java
@@ -0,0 +1,10 @@
+package lessonmaventren;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstJava {
+    @Test
+    public void firstTest(){
+        System.out.println("Первый проект");
+    }
+}
