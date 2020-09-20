bb6acad2d94cf0f86483b3cdef965564b27997f1
allinuxwork <altim4@yandex.ru>
2020-09-20 20:48:39 +0400

    доработал

--- java-oop-at-lessons_8_hw/src/javaoopatlessons_8_hw/jav/base/BeforeAndAfterStep.java
+++ java-oop-at-lessons_8_hw/src/javaoopatlessons_8_hw/jav/base/BeforeAndAfterStep.java
@@ -14,26 +14,29 @@ import java.util.concurrent.TimeUnit;
 public abstract class BeforeAndAfterStep {
 
 	public WebDriver driver;
+	public WebDriverWait wait15second;
 
 	@BeforeEach
 	public void baseTestBeforeAll() {
 		WebDriverManager.chromedriver().setup();
+//		System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--disable-notifications");
 		options.addArguments("--disable-popup-blocking");
 		options.setExperimentalOption("excludeSwitches",
 			Collections.singletonList("disable-popup-blocking"));
 		options.addArguments("--no-sandbox");
-		driver = new ChromeDriver(options);
+		driver = new ChromeDriver(options);	
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
+		wait15second = new WebDriverWait(driver, 15);
 	}
 
 	@AfterEach
 	public void BaseTestAfterAll() {
+		System.out.println("Tест завершен");
 		driver.quit();
 	}
-
 }
